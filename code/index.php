0062d4ab51cb7a2411f0431dfde09aaa141acb62
luismiguel.saez <luismiguel.saez@madisonmk.com>
2017-10-03 16:43:41 +0200

    Fixed Slim closure args

--- code/index.php
+++ code/index.php
@@ -4,8 +4,8 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
-$app->group('/api', function ($req, $res, $args) {
-	$this->get('/test', function ($req, $res, $args) {
+$app->group('/api', function ($req, $res) {
+	$this->get('/test', function ($req, $res) {
 		$body = "Test API";
 		$modres = $res->withStatus(200)
 					  ->withHeader("Content-Type","application/json")
