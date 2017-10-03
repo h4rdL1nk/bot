925547e7d42ab7504a4fed4ff84f0e3a6c4d5ffb
luismiguel.saez <luismiguel.saez@madisonmk.com>
2017-10-03 16:50:00 +0200

    Updated Slim sample app

--- code/index.php
+++ code/index.php
@@ -3,19 +3,7 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
-
-$app->group('/api', function ($req, $res) {
-	$this->get('/test', function ($req, $res) {
-		$body = "Test API";
-		$modres = $res->withStatus(200)
-					  ->withHeader("Content-Type","application/json")
-					  ->write($body);
-
-		return $modres;
-	});
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
 });
-
-
 $app->run();
-
-?>
