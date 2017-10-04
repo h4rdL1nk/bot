305f09b1b344b8c59cc68901b67d8125e6d6f818
luismiguel.saez <luismiguel.saez@madisonmk.com>
2017-10-04 11:24:00 +0200

    Added random string function

--- code/index.php
+++ code/index.php
@@ -3,7 +3,22 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, " . $name;
-});
+
+$app->get('/util/rand/string', function () {
+
+	$length = 20;
+    $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    $str = '';
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    if ($max < 1) {
+        throw new Exception('$keyspace must be at least two characters long');
+    }
+    for ($i = 0; $i < $length; ++$i) {
+        $str .= $keyspace[random_int(0, $max)];
+    }
+    echo $str;
+
+});	
+
 $app->run();
