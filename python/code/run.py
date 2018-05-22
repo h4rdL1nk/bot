4a0399c1d6311a90e7844647d38cb4f1946b03e7
h4rdL1nk <imthedanger@protonmail.com>
2018-05-22 12:54:20 +0200

    Added requirements.txt

--- python/code/run.py
+++ python/code/run.py
@@ -1,25 +1,31 @@
 import logging
+import os
+import requests
+import json
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 
 def main():
 
-    updater = Updater(token='425033555:AAFYf2UU2b7PLJYwPS0q6-jpEkmpUeFPq3M')
+    telegram_token = os.environ['TOKEN']
+
+    updater = Updater(token=telegram_token)
     dispatcher = updater.dispatcher
 
     #Declare handlers
     start_handler = CommandHandler('start', start)
+    whoami_handler = CommandHandler('whoami', whoami)
     echo_handler = MessageHandler(Filters.text, echo)
     unknown_handler = MessageHandler(Filters.command, unknown)
 
     #Start handlers
     dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(whoami_handler)
     dispatcher.add_handler(echo_handler)
     dispatcher.add_handler(unknown_handler)
 
     updater.start_polling()
-    
 
 def loggingSetup():
 
@@ -27,6 +33,17 @@ def loggingSetup():
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO
     )
+
+def whoami(bot, update):
+    r = requests.get('http://ifconfig.co/json')
+
+    bot.send_message(chat_id=update.message.chat_id, text="WHO")
+
+    if r.status_code == 200:
+        data = r.json()
+        bot.send_message(chat_id=update.message.chat_id, text=data['city'] + "(" + data['country'] + ") " + data['ip'])
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text="ifconfig.co returned bad HTTP_CODE :(")
 
 def unknown(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
